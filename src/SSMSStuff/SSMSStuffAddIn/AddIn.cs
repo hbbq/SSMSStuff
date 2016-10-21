3e4340c285e56398e4efe5f8103db027e4dedf6d
henber <henber@GOT1139>
2016-10-21 10:37:45 +0200

    Initial work

---
+++ src/SSMSStuff/SSMSStuffAddIn/AddIn.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedGate.SIPFrameworkShared;
+using RedGate.SIPFrameworkShared.ObjectExplorer;
+using SSMSStuffAddIn.ObjectExplorerMenus;
+
+namespace SSMSStuffAddIn
+{
+
+    public class AddIn : ISsmsAddin4
+    {
+
+        public string Author => @"Henrik Bergqvist";
+
+        public string Description => Name;
+
+        public string Name => @"SSMS Stuff Add In";
+
+        public string Url => @"https://github.com/hbbq/SSMSStuff";
+
+        public string Version => @"1.0.0.0";
+        
+        private ISsmsFunctionalityProvider6 m_Provider;
+
+        public void OnLoad(ISsmsExtendedFunctionalityProvider provider)
+        {
+            m_Provider = (ISsmsFunctionalityProvider6)provider;
+            AddObjectExplorerContextMenu();
+            AddObjectExplorerListener();
+        }
+
+        private void AddObjectExplorerListener()
+        {
+            m_Provider.ObjectExplorerWatcher.ConnectionsChanged += (args) => { OnConnectionsChanged(args); };
+            m_Provider.ObjectExplorerWatcher.SelectionChanged += (args) => { OnSelectionChanged(args); };
+        }
+
+        private void AddObjectExplorerContextMenu()
+        {
+            m_Provider.AddTopLevelMenuItem(new AssemblyExportMenuItem(m_Provider));
+        }
+
+        private void OnSelectionChanged(ISelectionChangedEventArgs args)
+        {
+            //m_MessageLog.AddMessage(string.Format("Object explorer selection: {0}", args.Selection.Path));
+        }
+
+        private void OnConnectionsChanged(IConnectionsChangedEventArgs args)
+        {
+            //m_MessageLog.AddMessage("Object explorer connections:");
+            int count = 1;
+            foreach (var connection in args.Connections)
+            {
+                //m_MessageLog.AddMessage(string.Format("\t{0}: {1}", count, connection.Server));
+                count++;
+            }
+        }
+
+        public void OnNodeChanged(ObjectExplorerNodeDescriptorBase node)
+        {
+        }
+
+        public void OnShutdown()
+        {
+        }
+
+    }
+
+}
---
+++ src/SSMSStuff/SSMSStuffAddIn/ObjectExplorerMenus/AssemblyExportMenuItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedGate.SIPFrameworkShared;
+
+namespace SSMSStuffAddIn.ObjectExplorerMenus
+{
+
+    class AssemblyExportMenuItem : ActionSimpleOeMenuItemBase
+    {
+
+        private readonly ISsmsFunctionalityProvider6 m_Provider;
+
+        public AssemblyExportMenuItem(ISsmsFunctionalityProvider6 provider)
+        {
+            m_Provider = provider;
+        }
+
+        public override string ItemText => "Export to file...";
+
+        public override bool AppliesTo(ObjectExplorerNodeDescriptorBase oeNode)
+        {
+            return ((oeNode as IOeNode)?.Type ?? "") == "SqlAssembly";
+        }
+
+        public override void OnAction(ObjectExplorerNodeDescriptorBase node)
+        {
+
+            var oeNode = node as IOeNode;
+
+            if (oeNode == null) return;
+
+            IDatabaseObjectInfo db;
+            IConnectionInfo cn;
+
+            if(oeNode.TryGetDatabaseObject(out db) && oeNode.TryGetConnection(out cn))
+            {
+
+                m_Provider.QueryWindow.OpenNew($"select * from sys.assemblies where [name] = '{oeNode.Name.Replace("'", "''")}'", oeNode.Name, cn.ConnectionString);
+                
+
+            }            
+
+        }
+
+    }
+
+}
